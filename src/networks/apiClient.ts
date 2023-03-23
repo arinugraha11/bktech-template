cb84100e1ab2ed22c459ececcd55e35d107ed7f8
arinugraha11 <ari.nugraha@tokopedia.com>
2023-03-23 22:08:13 +0700

    feat: setup interceptor authorization header

--- src/networks/apiClient.ts
+++ src/networks/apiClient.ts
@@ -10,6 +10,16 @@ const axiosClient = axios.create({
 });
 
 // If there are authentication, use this interceptor
-// axiosClient.interceptors.response.use();
+axiosClient.interceptors.request.use((config) => {
+  console.log("999 ini token");
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNzg2NjNiLWY4ZDktNDY1ZC1iYWI0LTM1Mjc1ZjdiNjY2ZiIsImVtYWlsIjoiYXJpOTlAZ21haWwuY29tIiwibmFtZSI6ImFyaTEyMyIsInBob25lX251bWJlciI6bnVsbCwiaW1hZ2VfdXJsIjpudWxsLCJpYXQiOjE2Nzk1ODMwNjUsImV4cCI6MTY3OTU5Mzg2NX0.gvwe7cCcJEnArW9QMT4krLpm2GPGzAYEVa4t90ejPh4";
+  config.headers = config.headers || {};
+
+  if (token) {
+    config.headers.Authorization = `token ${token}`;
+  }
+  return config;
+});
 
 export default axiosClient;
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import useGetAllUser from "../../hooks/useGetAllUser";
 import { ColoredDiv } from "./styles";
 
 const Home = () => {
+  const { listUser } = useGetAllUser();
+
+  console.log("[List User]", listUser);
+
   return (
     <div>
       <h3>This is homepage</h3>
