5d4435fb9893bc67563ad2e165ab4d70db7fb181
arinugraha11 <ari.nugraha@tokopedia.com>
2023-03-23 14:16:38 +0700

    feat: add private route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import PrivateRoutes from "../components/PrivateRoutes";
+import About from "./About";
 
 // Add your components here
-import About from "./About";
+import Login from "./Auth/Login";
 import Home from "./Home";
 
 const MainRoutes = () => {
   return (
     <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/about" element={<About />} />
+      </Route>
+
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
 
       {/* Add more routes below */}
     </Routes>
